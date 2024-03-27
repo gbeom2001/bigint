8a5a9943705930f95babaf47929aa589450eaddf
박경범 <pbum2001@gmail.com>
2024-03-27 11:45:01 +0900

    최초파일

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+string first;
+string second;
+string sum;
+
+int f=0;
+int s=0;
+
+cout << "First number  >>";
+cin >> first;
+cout << "Second number  >>";
+cin >> second;
+
+f=first.length();
+s=second.length();
+
+if(f>s)
+{
+for(int i=1;i=<s;i++)
+{
+char e =first[f-i];
+x=atoi(e);
+}
+}
+
+
+
+
+
+
+}
