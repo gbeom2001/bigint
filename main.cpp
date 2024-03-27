688fce23c9437f9fbdd94a213c83701832548145
박경범 <pbum2001@gmail.com>
2024-03-27 11:55:11 +0900

    9칸 씩 배열

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
 string first;
 string second;
 string sum;
+string sub;
 
 int f=0;
 int s=0;
@@ -19,19 +20,129 @@ cin >> second;
 
 f=first.length();
 s=second.length();
+int fn = f%9;
+int sn = s%9;
+int fm = f/9;
+int sm = s/9;
 
-if(f>s)
+if(f<=9&&s<=9)
 {
-for(int i=1;i=<s;i++)
+    sum=stoi(first)+stoi(second);
+    sub=stoi(first)-stoi(second);
+    cout << "sum        >>" << sum << endl;
+    cout << "sub        >>" << sub << endl;
+
+    return;
+}
+
+else
 {
-char e =first[f-i];
-x=atoi(e);
+    int fc[100];
+    int i=1;
+    if(fn!=0)
+    {
+    
+    fc[0]=stoi(first.substr(0, fn));
+    }
+    else i--;
+
+    for( i;i<=fm;i++)
+    {
+        fc[i]=stoi(first.substr(0, 9));
+    }
+//
+    int sc[100];
+    int j=1;
+    if(sn!=0)
+    {
+    
+    sc[0]=stoi(second.substr(0, 9));
+    }
+    else i--;
+
+    for( j;j<=sm;j++)
+    {
+        sc[i]=stoi(second.substr(0, 9));
+    }
+//
+    int total[100];
+    int mtotal[100];
+    if(i==j)
+    {
+        for(i;i>=0;i--)
+        {
+            total[i]=fc[i]+sc[i];
+        
+            if(total[i]>1000000000 && i!=0)
+            {
+                total[i-1]=total[i-1]+1;
+                total[i]=total[i]-1000000000;
+            }
+            if(fc[0]>sc[0])
+            {
+                mtotal[i]=fc[i]-sc[i];
+                mtotal[i-1]=mtotal[i-1]-1;
+                mtotal[i]=mtotal[i]+1000000000;
+
+            }
+            if(fc[0]<sc[0])
+            {
+                mtotal[i]=sc[i]-fc[i];
+                mtotal[i-1]=mtotal[i-1]-1;
+                mtotal[i]=mtotal[i]+1000000000;
+
+            }
+
+        
+        }
+
+    }
+    if(i>j)
+    {
+
+        int k = i-j;
+        for(i;i>=0;i--)
+        {
+            total[i]=fc[i]+sc[i];
+            mtotal[i]=fc[i]-sc[i];
+            if(total[i]>1000000000 && i!=0)
+            {
+                total[i-1]=total[i-1]+1;
+                total[i]=total[i]-1000000000;
+            }
+
+            if(mtotal[i]<0)
+            {
+                mtotal[i-1]=mtotal[i-1]-1;
+                mtotal[i]=mtotal[i]+1000000000;
+
+            }
+        }
+
+    }
+    if(i<j)
+    {
+        for(i;i>=0;i--)
+        {
+            total[i]=fc[i]+sc[i];
+            mtotal[i]=sc[i]-fc[i];
+            if(total[i]>1000000000 && && i!=0)
+            {
+                total[i-1]=total[i-1]+1;
+                total[i]=total[i]-1000000000;
+            }
+
+            if(mtotal[i]<0)
+            {
+                mtotal[i-1]=mtotal[i-1]-1;
+                mtotal[i]=mtotal[i]+1000000000;
+
+            }
+
+        }
+
+    }
+
 }
-}
-
-
-
-
-
 
 }
